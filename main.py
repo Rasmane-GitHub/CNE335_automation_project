020e48a9f0939d48bb6312a8502b3adc1554d5c6
Rasmane-GitHub <132719909+Rasmane-GitHub@users.noreply.github.com>
2023-11-24 18:12:27 -0800

    Updated code
    Automation

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
 
+# This is the template code for the CNE335 Final Project
+# Rasmane Sawadogo
+# CNE 335Fall
+
+
+import os
+import subprocess
+
+from Server import Server
+
+os.system("ping -n 1 3.3.11.1")
 def print_program_info():
     # TODO - Change your name
     print("Server Automator v0.1 by Your Name")
@@ -9,5 +17,31 @@ def print_program_info():
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
+    server_ip = "3.3.11.1"
+    server = Server(server_ip)
+
+
+
     # TODO - Create a Server object
     # TODO - Call Ping method and print the results
+
+class Server:
+    def __init__(self, server_ip):
+        self.server_ip = server_ip
+
+    def ping(self):
+        try:
+            # Use subprocess to run the ping command and capture the output
+            result = subprocess.check_output(["ping", "-n", "1", self.server_ip], text=True)
+            return result
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e}"
+
+
+
+
+        # TODO - Call the Ping method and print the results
+
+    ping_result = server.ping()
+    print(f"Ping result for {server_ip}:")
+    print(ping_result)
